2b218c49b62707a4530d72f26f7dffadecb6c791
dimcho <otvudnamaikatikura@gmail.com>
2017-01-05 21:30:24 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,10 +6,10 @@ from xlrd import open_workbook
 
 
 # DEFINES:
-master_file = 'PGiMaster.xls'
-container_all_files = "/home/dimcho/PycharmProjects/dzhrt/cis/PGI_all"
-extension = ".txt"
-version_pattern = "(^.*(REV|rev|Rev)\s\d\d+)"
+MASTER_FILE = 'PGiMaster.xls'
+CONTAINER_ALL_FILES = "/home/dzhrt/PycharmProjects/untitled/pyt/cisco_devices/PGI"
+EXTENSION = ".txt"
+VERSION_PATTERN = "(^.*(REV|rev|Rev)\s\d\d+)"
 
 
 def load_data_from_v_file(file_name):
@@ -26,7 +26,7 @@ def load_data_from_v_file(file_name):
     return data
 
 
-def cisco_vs_juniper_excel_entries(master_file):
+def cisco_vs_juniper_excel_entries(MASTER_FILE):
     """
     :param file_name: takes the path to the excel master file
     :return: 3 lists for cisco, juniper and other devices,
@@ -35,7 +35,7 @@ def cisco_vs_juniper_excel_entries(master_file):
     cisco_names = []
     juniper_names = []
     other_devices = []
-    for row in load_data_from_v_file(master_file):
+    for row in load_data_from_v_file(MASTER_FILE):
         device_type = row[3]
         system_name = row[4]
         if 'Cisco' in device_type:
@@ -55,7 +55,7 @@ def get_all_files(path_to_folder):
     result = []
     for root, dirs, files in os.walk(path_to_folder, topdown=False):
         for name in files:
-            if name.endswith(extension):
+            if name.endswith(EXTENSION):
                 result.append(str(os.path.join(root, name)))
     return result
 
@@ -68,8 +68,8 @@ def cisco_vs_juniper_files():
     indicating_string = "Hardware inventory:"
     cisco_file_list = []
     juniper_file_list = []
-    for file in get_all_files(container_all_files):
-        if file.endswith(extension):
+    for file in get_all_files(CONTAINER_ALL_FILES):
+        if file.endswith(EXTENSION):
             with open(file, "r") as f:
                 content = f.read()
                 if indicating_string in content:
@@ -84,7 +84,7 @@ def cisco_reader(dir_to_file):
     :param dir_to_file: path to cisco file be checked
     :return:  a list of tuples with PIDs and SNs: [(pid, sn), (pid, sn), (pid, sn)...]
     """
-    file_name = os.path.basename(dir_to_file).replace(extension, '')
+    file_name = os.path.basename(dir_to_file).replace(EXTENSION, '')
     result = []
     with open(dir_to_file, "r") as f:
         for line in f:
@@ -121,7 +121,7 @@ def juniper_reader(dir_to_file):
             line = line.split('   ')
             for i, item in enumerate(line):
                 try:
-                    if re.match(version_pattern, item):
+                    if re.match(VERSION_PATTERN, item):
                         pid, sn = line[i+1], line[i+2]
                         sn = line[i+2]
                 except Exception as e:
@@ -144,17 +144,33 @@ def create_dict(file_list, reader):
     """
     result = {}
     for file in file_list:
-        name = os.path.basename(file).replace(extension, '')
+        name = os.path.basename(file).replace(EXTENSION, '')
         pairs = reader(file)
         result[name] = pairs
     return result
 
 
-def write_missing_entries(missing_names_list, all_dict, wsheet, header):
+def write_to_excel_file(data, sheet, header):
+    """
+    :param data: list of lists for each row to be written in xls file
+    :param sheet: sheet to be written
+    :param header: format the header for columns in the excel
+    :return:
+    """
+    for row in range(0, len(data)):
+        for col in range(0, len(data[row])):
+            if row == 0:
+                sheet.write(row, col, data[row][col], header)
+            else:
+                sheet.write(row, col, data[row][col])
+    return 1
+
+
+def write_missing_entries(missing_names_list, all_dict, sheet, header):
     """
     This function writes a sheet in excel with the missing entries from the master
     :param missing_names_list: a list of the missing cisco or juniper entries
-    :param wsheet: the sheet in the excel file to be written
+    :param sheet: the sheet in the excel file to be written
     :param all_dict: a dict containing all the pid and SNs for cisco + juniper
     :return:
     """
@@ -165,85 +181,68 @@ def write_missing_entries(missing_names_list, all_dict, wsheet, header):
             for pid, sn in all_dict[name]:
                 missing_entries.append(["", pid, sn])
 
-    for row in range(0, len(missing_entries)):
-        for col in range(0, len(missing_entries[row])):
-            if row == 0:
-                wsheet.write(row, col, missing_entries[row][col], header)
-            else:
-                wsheet.write(row, col, missing_entries[row][col])
-    return 1
+    write_to_excel_file(missing_entries, sheet, header)
 
 
 def main():
-
+    # CREATING CISCO AND JUNIPER FILE LISTS
     cisco_file_list, juniper_file_list = cisco_vs_juniper_files()
-    cisco_names, juniper_names, other_devices = cisco_vs_juniper_excel_entries(master_file)
+    cisco_names, juniper_names, other_devices = cisco_vs_juniper_excel_entries(MASTER_FILE)
 
-    set_cisco_files = set([os.path.basename(a).replace(extension, '') for a in cisco_file_list])
-    set_juniper_files = set([os.path.basename(a).replace(extension, '') for a in juniper_file_list])
-    set_cisco_names = set([os.path.basename(a).replace(extension, '') for a in cisco_names])
-    set_juniper_names = set([os.path.basename(a).replace(extension, '') for a in juniper_names])
-    missing_files_for_cisco = set_cisco_names - set_cisco_files
-    missing_files_for_juniper = set_juniper_names - set_juniper_files
-    missing_names_for_cisco = set_cisco_files - set_cisco_names
-    missing_names_for_juniper = set_juniper_files - set_juniper_names
+    # CREATING SETS FOR CISCO AND JUNIPER TO FIND DIFF
+    set_cisco_files = set(os.path.basename(item).replace(EXTENSION, '') for item in cisco_file_list)
+    set_juniper_files = set(os.path.basename(item).replace(EXTENSION, '') for item in juniper_file_list)
+    missing_names_for_cisco = set_cisco_files - set(cisco_names)
+    missing_names_for_juniper = set_juniper_files - set(juniper_names)
 
-
-    cisco_dict = create_dict(cisco_file_list, cisco_reader)
-    juniper_dict = create_dict(juniper_file_list, juniper_reader)
-
-
+    # CREATING DICT WITH ALL ENTRIES FOR CISCO AND JUNIPER
     all_dict = {}
-    all_dict.update(cisco_dict)
-    all_dict.update(juniper_dict)
+    all_dict.update(create_dict(cisco_file_list, cisco_reader))
+    all_dict.update(create_dict(juniper_file_list, juniper_reader))
 
-
-    # Writing the updated file
+    # PREPARING TO WRITE NEW DATA
     combined_file_name = "PGiMaster_UPDATED.xlsx"
     workbook = xlsxwriter.Workbook(combined_file_name)
     worksheet = workbook.add_worksheet()
-    worksheet_c = workbook.add_worksheet("extra_cisco_files")
-    worksheet_j = workbook.add_worksheet("extra_juniper_files")
+    worksheet_2 = workbook.add_worksheet("extra_cisco_files")
+    worksheet_3 = workbook.add_worksheet("extra_juniper_files")
     format_header = workbook.add_format({'bold': True, 'font_color': 'black'})
-    master_data = load_data_from_v_file(master_file)
+
+    # CREATING THE UPDATED DATA FILE LIST TO BE WRITTEN
+    old_data = load_data_from_v_file(MASTER_FILE)
     updated_data = []
-    pre_list = ['' for i in range(8)]
-    post_list = ['' for i in range(7)]
-    for row in master_data:
-        system_name = row[4]
-        if system_name:
-            if system_name in all_dict.keys():
+    pre_row = ['' for i in range(8)]
+    post_row = ['' for i in range(7)]
+    for row in old_data:
+        SYSTEM_NAME = row[4]
+        COMPONENT = row[8]
+        COMPONENT_SERIAL = row[9]
+        if SYSTEM_NAME:
+            if SYSTEM_NAME in all_dict.keys():
                 updated_data.append(row)
-                for pair in all_dict[system_name]:
+                for pair in all_dict[SYSTEM_NAME]:
                     mid_list = [pair[0], pair[1]]
-                    updated_row = pre_list + mid_list + post_list
+                    updated_row = pre_row + mid_list + post_row
                     updated_data.append(updated_row)
-            elif system_name not in all_dict.keys():
-                if not row[8]:
-                    row[8] = "NO_FILE"
-                if not row[9]:
-                    row[9] = "NO_FILE"
+            elif SYSTEM_NAME not in all_dict.keys():
+                if not COMPONENT:
+                    COMPONENT = "NO_FILE"
+                if not COMPONENT_SERIAL:
+                    COMPONENT_SERIAL = "NO_FILE"
                 updated_data.append(row)
             else:
                 updated_data.append(row)
 
     # WRITING UPDATED FILE
-    for row in range(0, len(updated_data)):
-        for col in range(0, len(updated_data[row])):
-            if row == 0:
-                worksheet.write(row, col, updated_data[row][col], format_header)
-            else:
-                worksheet.write(row, col, updated_data[row][col])
-
+    write_to_excel_file(updated_data, worksheet, format_header)
 
     # WRITING MISSING CISCO ENTRIES
-    write_missing_entries(missing_names_for_cisco, all_dict, worksheet_c, format_header)
+    write_missing_entries(missing_names_for_cisco, all_dict, worksheet_2, format_header)
+    
     # WRITING MISSING JUNIPER ENTRIES
-    write_missing_entries(missing_names_for_juniper, all_dict, worksheet_j, format_header)
-    # MISSING CISCO ENTRIES
+    write_missing_entries(missing_names_for_juniper, all_dict, worksheet_3, format_header)
 
-
-    # OTHER DEVICES CHECK
+    # CHECK FOR OTHER DEVICES
     print "\n" + "="*50 + "\nChecking for non-Cisco non-Juniper devices..."
     other_devices_found = False
     for device in other_devices:
@@ -254,25 +253,15 @@ def main():
     if not other_devices_found:
         print "No other devices found"
     print "="*50
-
-
     # STATISTICS
-    print "\n" + "=" * 50
-    print "cisco files:", len(cisco_file_list)
-    print "juniper files:", len(juniper_file_list)
-    print "total count files:", len(cisco_file_list) + len(juniper_file_list)
-    print "=" * 50
-    print "\n" + "=" * 50
-    print "cisco entries in excel:", len(cisco_names)
-    print "juniper entries in excel:", len(juniper_names)
-    print "total count in excel:", len(cisco_names) + len(juniper_names)
-    print "=" * 50
-    print "\n" + "=" * 50
-    print "missing files for cisco:", len(missing_files_for_cisco)
-    print "missing files for juniper", len(missing_files_for_juniper)
-    print "extra files for cisco:", len(missing_names_for_cisco)
-    print "extra files for juniper:", len(missing_names_for_juniper)
-    print "=" * 50
+    print "\n"+"="*50
+    print "cisco files: {}".format(len(cisco_file_list))
+    print "juniper files: {}".format(len(juniper_file_list))
+    print "="*50
+    print "\n"+"="*50
+    print "cisco entries in excel: {}".format(len(cisco_names))
+    print "juniper entries in excel: {}".format(len(juniper_names))
+    print "="*50
 
 
 if __name__ == '__main__':
